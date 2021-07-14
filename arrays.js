46840dcd1cd4e0b403f9e8abcebae5d7dac334a1
GrayHead <ss.zhuravlov@gmail.com>
2021-07-14 19:37:57 +0300

    add arrays.js

---
+++ arrays.js
@@ -0,0 +1,116 @@
+let coursesTitleArray = [
+	'JavaScript Complex',
+	'Java Complex',
+	'Python Complex',
+	'QA Complex',
+	'Fullstack',
+	'Frontend'
+];
+let coursesAndDurationArray = [
+	{title: 'JavaScript Complex', monthDuration: 5},
+	{title: 'Java Complex', monthDuration: 6},
+	{title: 'Python Complex', monthDuration: 6},
+	{title: 'QA Complex', monthDuration: 4},
+	{title: 'FullStack', monthDuration: 7},
+	{title: 'Frontend', monthDuration: 4}
+];
+let coursesArray = [
+	{
+		title: 'JavaScript Complex',
+		monthDuration: 5,
+		hourDuration: 909,
+		modules: ['html', 'css', 'js', 'mysql', 'mongodb', 'react', 'angular', 'aws', 'docker', 'git', 'node.js']
+	},
+	{
+		title: 'Java Complex',
+		monthDuration: 6,
+		hourDuration: 909,
+		modules: ['html', 'css', 'js', 'mysql', 'mongodb', 'react', 'angular', 'aws', 'docker', 'git', 'java core', 'java advanced']
+	},
+	{
+		title: 'Python Complex',
+		monthDuration: 6,
+		hourDuration: 909,
+		modules: ['html', 'css', 'js', 'mysql', 'mongodb', 'react', 'angular', 'aws', 'docker', 'git', 'python core', 'python advanced']
+	},
+	{
+		title: 'QA Complex',
+		monthDuration: 4,
+		hourDuration: 909,
+		modules: ['html', 'css', 'js', 'mysql', 'mongodb', 'react', 'angular', 'aws', 'docker', 'git', 'QA/QC']
+	},
+	{
+		title: 'FullStack',
+		monthDuration: 7,
+		hourDuration: 909,
+		modules: ['html', 'css', 'js', 'mysql', 'mongodb', 'react', 'angular', 'aws', 'docker', 'git', 'node.js', 'python', 'java']
+	},
+	{
+		title: 'Frontend',
+		monthDuration: 4,
+		hourDuration: 909,
+		modules: ['html', 'css', 'js', 'mysql', 'mongodb', 'react', 'angular', 'aws', 'docker', 'git', 'sass']
+	}
+];
+let courses = [
+	{
+		title: 'JavaScript Complex',
+		monthDuration: 5,
+		hourDuration: 909,
+		modules: ['html', 'css', 'js', 'mysql', 'mongodb', 'react', 'angular', 'aws', 'docker', 'git', 'node.js'],
+		logo: '',
+		price: 0,
+		rating: 5,
+		avgResult: 99
+	},
+	{
+		title: 'Java Complex',
+		monthDuration: 6,
+		hourDuration: 909,
+		modules: ['html', 'css', 'js', 'mysql', 'mongodb', 'react', 'angular', 'aws', 'docker', 'git', 'java core', 'java advanced'],
+		logo: '',
+		price: 0,
+		rating: 4.998,
+		avgResult: 97
+	},
+	{
+		title: 'Python Complex',
+		monthDuration: 6,
+		hourDuration: 909,
+		modules: ['html', 'css', 'js', 'mysql', 'mongodb', 'react', 'angular', 'aws', 'docker', 'git', 'python core', 'python advanced'],
+		logo: '',
+		price: 0,
+		rating: 4.812,
+		avgResult: 98
+	},
+	{
+		title: 'QA Complex',
+		monthDuration: 4,
+		hourDuration: 909,
+		modules: ['html', 'css', 'js', 'mysql', 'mongodb', 'react', 'angular', 'aws', 'docker', 'git', 'QA/QC'],
+		logo: '',
+		price: 0,
+		rating: 4.65,
+		avgResult: 97
+	},
+	{
+		title: 'FullStack',
+		monthDuration: 7,
+		hourDuration: 909,
+		modules: ['html', 'css', 'js', 'mysql', 'mongodb', 'react', 'angular', 'aws', 'docker', 'git', 'node.js', 'python', 'java'],
+		logo: '',
+		price: 0,
+		rating: 4.772,
+		avgResult: 100
+	},
+	{
+		title: 'Frontend',
+		monthDuration: 4,
+		hourDuration: 909,
+		modules: ['html', 'css', 'js', 'mysql', 'mongodb', 'react', 'angular', 'aws', 'docker', 'git', 'sass'],
+		logo: '',
+		price: 0,
+		rating: 4.53,
+		avgResult: 90
+	}
+];
