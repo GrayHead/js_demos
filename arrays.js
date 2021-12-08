7c99c201add0316eab86a9fe7cecd3742f41ac72
GrayHead <ss.zhuravlov@gmail.com>
2021-12-08 15:39:20 +0200

    add lesson 11 task

--- arrays.js
+++ arrays.js
@@ -460,3 +460,17 @@ let user = {
     skills: [{title: 'java', exp: 10}, {title: 'js', exp: 10}, {title: 'c++', exp: 10}],
     car: {model: 'toyota', year: 2021, power: 250}
 }
+
+let users = [
+    {name: 'vasya', age: 31, status: false},
+    {name: 'petya', age: 30, status: true},
+    {name: 'kolya', age: 29, status: true},
+    {name: 'olya', age: 28, status: false},
+    {name: 'max', age: 30, status: true},
+    {name: 'anya', age: 31, status: false},
+    {name: 'oleg', age: 28, status: false},
+    {name: 'andrey', age: 29, status: true},
+    {name: 'masha', age: 30, status: true},
+    {name: 'olya', age: 31, status: false},
+    {name: 'max', age: 31, status: true}
+];
