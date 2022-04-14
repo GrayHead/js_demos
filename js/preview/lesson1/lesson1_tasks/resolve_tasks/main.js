a09131bc6c12835e1e9b02559a5c6b9db476cd01
GrayHead <ss.zhuravlov@gmail.com>
2022-04-14 19:29:01 +0300

    js hw 1 resolve

---
+++ js/preview/lesson1/lesson1_tasks/resolve_tasks/main.js
@@ -0,0 +1,103 @@
+// - Створити змінні. Присвоїти кожному з них значення:
+// 'hello','owu','com', 'ua', 1, 10, -999, 123, 3.14, 2.7, 16, true, false.
+//     Вивести кожну змінну за допомогою: console.log , alert, document.write
+// let a1 = 'hello';
+// let a2 = 'owu';
+// let a3 = 'com';
+// let a4 = 'ua';
+// let a5 = 1;
+// let a6 = 10;
+// let a7 = -999;
+// let a8 = 123;
+// let a9 = 3.14;
+// let a10 = 2.7;
+// let a11 = 16;
+// let a12 = true;
+// let a13 = false;
+// console.log(a1);
+// console.log(a2);
+// console.log(a3);
+// console.log(a4);
+// console.log(a5);
+// console.log(a6);
+// console.log(a7);
+// console.log(a8);
+// console.log(a9);
+// console.log(a10);
+// console.log(a11);
+// console.log(a12);
+// console.log(a13);
+
+// - Створити об'єкт book з наступними полями :
+// назва, (тип string)
+// кількість сторінок (числовий тип),
+// жанр (string)
+// let book1 = {
+//     title: 'LOTR',
+//     pages: 345,
+//     genre: 'fantasy'
+// };
+// console.log(book1);
+
+// - Створити об'єкт book з наступними полями :
+// назва, (тип string)
+// кількість сторінок (числовий тип),
+// жанр (string)
+// автори (тип - масив, кожен елемент масиву - це стрінга)
+// let book2 = {
+//     title: 'asywqef',
+//     pages: 123,
+//     genre: 'ufyq',
+//     authors: [
+//         'asufdhgasfd',
+//         'yrytry',
+//         'ywytfqwe'
+//     ]
+// }
+//
+// console.log(book2);
+
+// - Створити 3 змінних firstName, middleName, lastName,
+// наповнити їх своїм ПІБ. З'єднати їх в одну змінну person
+// (Не об'єкт, просто за допомоги конкатенації)
+// let fn = 'Serhii';
+// let mn = 'Serhiiyovich';
+// let ln = 'Zhuravlov';
+//
+// // let person = fn + ' ' + mn + ' ' + ln;
+//
+// let person = `${fn} ${mn} ${ln}`;
+// console.log(person);
+
+// - За допомогою 3х різних prompt() отримати 3 слова які являються вашими
+// Ім`ям, По-Батькові та роками. та вивести в консоль
+// let fn = prompt('enter you first name');
+// let mn = prompt('enter you middle name');
+// let ln = prompt('enter you last name');
+// let person = `${fn} ${mn} ${ln}`;
+// console.log(person);
+
+// - За допомогою оператора typeof визначити типи наступних змінних та вивести їх в консоль.
+//     let a = 100; let b = '100'; let c = true;
+// let a = 100;
+// let b = '100';
+// let c = true;
+// console.log(typeof a);
+// console.log(typeof b);
+// console.log(typeof c);
+
+// - Створити масив об'єктів з назвою books з наступними полями
+// - назва, кількість сторінок, жанр, автори. Вивести кожну книгу як окремий об'єкт
+//
+// let books = [
+//     {title: 'asd1', pages: 10, genre: 'drama', authors: ['asd', 'wqe', 'fdgdfg']},
+//     {title: 'asd2', pages: 101, genre: 'fantasy', authors: ['asd', 'wqe', 'fdgdfg']},
+//     {title: 'asd3', pages: 102, genre: 'drama', authors: ['asd', 'wqe', 'fdgdfg']},
+//     {title: 'asd4', pages: 104, genre: 'fantasy', authors: ['asd', 'wqe', 'fdgdfg']}
+// ];
+//
+// console.log(books[0]);
+// console.log(books[1]);
+// console.log(books[2]);
+// console.log(books[3]);
+
